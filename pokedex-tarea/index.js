34ef213278bf8c0bcd5e3b538d3f777bc0896d01
TomasHaydu <103974880+TomasHaydu@users.noreply.github.com>
2022-07-04 14:44:24 -0300

    Add files via upload

--- pokedex-tarea/index.js
+++ pokedex-tarea/index.js
@@ -1,28 +1,53 @@
 function llamadaAPI() {
-    return fetch("https://pokeapi.co/api/v2/pokemon")
-      .then((r) => r.json())
-      .then((rJSON) => nombrePokemon(rJSON.results))
-      .catch((error) => console.log("FALLÓ" + error));
-  }
-  
-  function nombrePokemon (pokemones) {
-    pokemones.forEach((pokemon) => {
-        const nombrePokemon = pokemon.name
-        const button = document.createElement('button')
-        button.type = "button"
-        button.className = "list-group-item list-group-item-action"
-        button.innerText = nombrePokemon
-        button.id = [nombrePokemon]
-        document.querySelector("#listaPokemones").append(button)
-    });
-}  
-nombrePokemon(llamadaAPI())
-
-document.querySelectorAll('button').forEach(function ($nombre){
-    $nombre.onclick = buttonClick()
-})
-
-function buttonClick (e){
-    const $button = e.target
-    $button.className = "list-group-item list-group-item-action active"
+  fetch("https://pokeapi.co/api/v2/pokemon")
+    .then((r) => r.json())
+    .then((rJSON) => nombrePokemon(rJSON.results))
+    .catch((error) => console.log("FALLÓ" + error));
 }
+
+async function nombrePokemon(pokemones) {
+  await pokemones.forEach((pokemon) => {
+    const nombrePokemon = pokemon.name;
+    const urlPokemon = pokemon.url;
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "list-group-item list-group-item-action";
+    button.innerText = nombrePokemon;
+    button.id = urlPokemon;
+    document.querySelector("#listaPokemones").append(button);
+    clickPokemon();
+  });
+}
+
+llamadaAPI();
+
+function clickPokemon() {
+  document.querySelectorAll("button").forEach(function ($botonNombre) {
+    $botonNombre.addEventListener("click", buttonClick);
+  });
+}
+
+async function buttonClick(e) {
+  const $button = e.target;
+  document.querySelectorAll("button").forEach(function (cadaBoton) {
+    cadaBoton.className = "list-group-item list-group-item-action";
+  });
+  $button.className = "list-group-item list-group-item-action active";
+  $button["aria-current"] = "true";
+
+
+  fetch ($button.id)
+  .then((r) => r.json())
+  .then ((rJSON) => 
+    (rJSON.abilities).forEach(function (habilidad){
+        const caracteristica = document.createElement('li')
+        caracteristica.innerHTML = "<strong>Ability: </strong>" + habilidad.ability.name
+        caracteristica.className = "list-group-item"
+        document.querySelector("#caracteristicas").append(caracteristica)
+    }))
+    .catch((error) => console("FALLO" + error))
+
+
+
+
+  }
